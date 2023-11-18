5be3f521ebfac02ddfac680a2efa8fd4e2dd664b
Ori Blanka <oriblanka305@gmail.com>
2023-11-18 18:19:32 +0200

    Implement 2 steps - capture and detection

--- FaceDetectionAndRecognition/FaceDetectionAndRecognition/Form1.Designer.cs
+++ FaceDetectionAndRecognition/FaceDetectionAndRecognition/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,121 +23,125 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            picCapture = new PictureBox();
-            caputureBtn = new Button();
-            detectFacesBtn = new Button();
-            personNameTxtBox = new TextBox();
-            addPersonBtn = new Button();
-            trainBtn = new Button();
-            recognizeBtn = new Button();
-            ditectedPic = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)picCapture).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)ditectedPic).BeginInit();
-            SuspendLayout();
+            this.capturePic = new System.Windows.Forms.PictureBox();
+            this.captureBtn = new System.Windows.Forms.Button();
+            this.detectBtn = new System.Windows.Forms.Button();
+            this.addPersonBtn = new System.Windows.Forms.Button();
+            this.trainBtn = new System.Windows.Forms.Button();
+            this.recognizeBtn = new System.Windows.Forms.Button();
+            this.personNameTxtBox = new System.Windows.Forms.TextBox();
+            this.detectedPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.capturePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detectedPic)).BeginInit();
+            this.SuspendLayout();
             // 
-            // picCapture
+            // capturePic
             // 
-            picCapture.Location = new Point(12, 12);
-            picCapture.Name = "picCapture";
-            picCapture.Size = new Size(749, 492);
-            picCapture.TabIndex = 0;
-            picCapture.TabStop = false;
+            this.capturePic.Location = new System.Drawing.Point(13, 13);
+            this.capturePic.Name = "capturePic";
+            this.capturePic.Size = new System.Drawing.Size(612, 425);
+            this.capturePic.TabIndex = 0;
+            this.capturePic.TabStop = false;
             // 
-            // caputureBtn
+            // captureBtn
             // 
-            caputureBtn.Location = new Point(805, 12);
-            caputureBtn.Name = "caputureBtn";
-            caputureBtn.Size = new Size(166, 31);
-            caputureBtn.TabIndex = 1;
-            caputureBtn.Text = "1. Capture";
-            caputureBtn.UseVisualStyleBackColor = true;
+            this.captureBtn.Location = new System.Drawing.Point(648, 13);
+            this.captureBtn.Name = "captureBtn";
+            this.captureBtn.Size = new System.Drawing.Size(140, 32);
+            this.captureBtn.TabIndex = 1;
+            this.captureBtn.Text = "1. Capture";
+            this.captureBtn.UseVisualStyleBackColor = true;
+            this.captureBtn.Click += new System.EventHandler(this.captureBtn_Click);
             // 
-            // detectFacesBtn
+            // detectBtn
             // 
-            detectFacesBtn.Location = new Point(805, 49);
-            detectFacesBtn.Name = "detectFacesBtn";
-            detectFacesBtn.Size = new Size(166, 31);
-            detectFacesBtn.TabIndex = 1;
-            detectFacesBtn.Text = "2. Detect Faces";
-            detectFacesBtn.UseVisualStyleBackColor = true;
-            // 
-            // personNameTxtBox
-            // 
-            personNameTxtBox.Location = new Point(805, 262);
-            personNameTxtBox.Name = "personNameTxtBox";
-            personNameTxtBox.Size = new Size(166, 23);
-            personNameTxtBox.TabIndex = 2;
+            this.detectBtn.Location = new System.Drawing.Point(648, 51);
+            this.detectBtn.Name = "detectBtn";
+            this.detectBtn.Size = new System.Drawing.Size(140, 32);
+            this.detectBtn.TabIndex = 1;
+            this.detectBtn.Text = "2. Detect Faces";
+            this.detectBtn.UseVisualStyleBackColor = true;
+            this.detectBtn.Click += new System.EventHandler(this.detectBtn_Click);
             // 
             // addPersonBtn
             // 
-            addPersonBtn.Location = new Point(805, 306);
-            addPersonBtn.Name = "addPersonBtn";
-            addPersonBtn.Size = new Size(166, 31);
-            addPersonBtn.TabIndex = 1;
-            addPersonBtn.Text = "3. Add Person";
-            addPersonBtn.UseVisualStyleBackColor = true;
+            this.addPersonBtn.Location = new System.Drawing.Point(648, 272);
+            this.addPersonBtn.Name = "addPersonBtn";
+            this.addPersonBtn.Size = new System.Drawing.Size(140, 32);
+            this.addPersonBtn.TabIndex = 1;
+            this.addPersonBtn.Text = "3. Add Person";
+            this.addPersonBtn.UseVisualStyleBackColor = true;
             // 
             // trainBtn
             // 
-            trainBtn.Location = new Point(805, 343);
-            trainBtn.Name = "trainBtn";
-            trainBtn.Size = new Size(166, 31);
-            trainBtn.TabIndex = 1;
-            trainBtn.Text = "4. Train Images";
-            trainBtn.UseVisualStyleBackColor = true;
+            this.trainBtn.Location = new System.Drawing.Point(648, 310);
+            this.trainBtn.Name = "trainBtn";
+            this.trainBtn.Size = new System.Drawing.Size(140, 32);
+            this.trainBtn.TabIndex = 1;
+            this.trainBtn.Text = "4. Train Images";
+            this.trainBtn.UseVisualStyleBackColor = true;
             // 
             // recognizeBtn
             // 
-            recognizeBtn.Location = new Point(805, 394);
-            recognizeBtn.Name = "recognizeBtn";
-            recognizeBtn.Size = new Size(166, 31);
-            recognizeBtn.TabIndex = 1;
-            recognizeBtn.Text = "5. Recognize Person";
-            recognizeBtn.UseVisualStyleBackColor = true;
+            this.recognizeBtn.Location = new System.Drawing.Point(648, 348);
+            this.recognizeBtn.Name = "recognizeBtn";
+            this.recognizeBtn.Size = new System.Drawing.Size(140, 32);
+            this.recognizeBtn.TabIndex = 1;
+            this.recognizeBtn.Text = "5. Recognize Persons";
+            this.recognizeBtn.UseVisualStyleBackColor = true;
             // 
-            // ditectedPic
+            // personNameTxtBox
             // 
-            ditectedPic.Location = new Point(805, 86);
-            ditectedPic.Name = "ditectedPic";
-            ditectedPic.Size = new Size(166, 170);
-            ditectedPic.TabIndex = 3;
-            ditectedPic.TabStop = false;
+            this.personNameTxtBox.Location = new System.Drawing.Point(648, 246);
+            this.personNameTxtBox.Name = "personNameTxtBox";
+            this.personNameTxtBox.Size = new System.Drawing.Size(140, 20);
+            this.personNameTxtBox.TabIndex = 2;
+            // 
+            // detectedPic
+            // 
+            this.detectedPic.Location = new System.Drawing.Point(648, 100);
+            this.detectedPic.Name = "detectedPic";
+            this.detectedPic.Size = new System.Drawing.Size(140, 129);
+            this.detectedPic.TabIndex = 3;
+            this.detectedPic.TabStop = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(983, 516);
-            Controls.Add(ditectedPic);
-            Controls.Add(personNameTxtBox);
-            Controls.Add(recognizeBtn);
-            Controls.Add(trainBtn);
-            Controls.Add(addPersonBtn);
-            Controls.Add(detectFacesBtn);
-            Controls.Add(caputureBtn);
-            Controls.Add(picCapture);
-            Name = "Form1";
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)picCapture).EndInit();
-            ((System.ComponentModel.ISupportInitialize)ditectedPic).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.detectedPic);
+            this.Controls.Add(this.personNameTxtBox);
+            this.Controls.Add(this.recognizeBtn);
+            this.Controls.Add(this.trainBtn);
+            this.Controls.Add(this.addPersonBtn);
+            this.Controls.Add(this.detectBtn);
+            this.Controls.Add(this.captureBtn);
+            this.Controls.Add(this.capturePic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.capturePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detectedPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
-        private PictureBox picCapture;
-        private Button caputureBtn;
-        private Button detectFacesBtn;
-        private TextBox personNameTxtBox;
-        private Button addPersonBtn;
-        private Button trainBtn;
-        private Button recognizeBtn;
-        private PictureBox ditectedPic;
+        private System.Windows.Forms.PictureBox capturePic;
+        private System.Windows.Forms.Button captureBtn;
+        private System.Windows.Forms.Button detectBtn;
+        private System.Windows.Forms.Button addPersonBtn;
+        private System.Windows.Forms.Button trainBtn;
+        private System.Windows.Forms.Button recognizeBtn;
+        private System.Windows.Forms.TextBox personNameTxtBox;
+        private System.Windows.Forms.PictureBox detectedPic;
     }
 }
+
--- FaceDetectionAndRecognition/FaceDetectionAndRecognition/Form1.cs
+++ FaceDetectionAndRecognition/FaceDetectionAndRecognition/Form1.cs
@@ -1,17 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
 
 namespace FaceDetectionAndRecognition
 {
     public partial class Form1 : Form
     {
         #region Variables
+        private Capture videoCapture = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        Mat frame = new Mat();
+        private bool faceDetectionEnabled = false;
+        CascadeClassifier faceCascadeClassifier = new CascadeClassifier(@"C:\haarcascades\haarcascade_frontalface_alt_tree.xml");
 
-        #endregion 
-
+        #endregion
         public Form1()
         {
             InitializeComponent();
-
         }
 
+        private void captureBtn_Click(object sender, EventArgs e)
+        {
+            videoCapture = new Capture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            // Step 1: Video Capture
+            videoCapture.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(capturePic.Width, capturePic.Height, Inter.Cubic);
+
+            // Step 2: Face Detection
+            if (faceDetectionEnabled)
+            {
+                // Convert the Bgr to Gray Image
+                Mat grayImage = new Mat();
+                CvInvoke.CvtColor(currentFrame, grayImage, ColorConversion.Bgr2Gray);
+                // Enhance the image to get better result
+                CvInvoke.EqualizeHist(grayImage, grayImage);
+
+                Rectangle[] faces = faceCascadeClassifier.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+                // If faces detected
+                if (faces.Length > 0)
+                {
+                    foreach (var face in faces)
+                    {
+                        // Draw squrae around each face
+                        CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Red).MCvScalar, 2);
+                    }
+                }
+            }
+
+            // Render the video into the Picture Box capturePic
+            capturePic.Image = currentFrame.Bitmap;
+        }
+
+        private void detectBtn_Click(object sender, EventArgs e)
+        {
+            faceDetectionEnabled = true;
+
+        }
     }
 }
